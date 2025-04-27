0c2694e8f2d1da9f72f8358b95c42f13adb73c4d
Zoviet <alexandr@asustem.ru>
2025-04-27 23:14:15 +0400

    fix options

--- test.lua
+++ test.lua
@@ -8,6 +8,5 @@ if not err then print(json.encode(res)) else print(json.encode(res)..err) end
 res,err = api.route({{13.428555,52.523219},{13.428545,52.523249}},'walking')
 if not err then print(json.encode(res)) else print(json.encode(res)..err) end
 
-
-
-
+res,err = api.table({{50.1169,53.1912},{50.1661,53.2172},{50.0839,53.1881},{50.128,53.1985},{50.1057,53.1846}})
+if not err then print(json.encode(res)) else print(json.encode(res)..err) end
